03dc894d3ee7ea3bff0c8134783b3a2e1e696dce
Devendra Ingale <ingale.devendra20@gmail.com>
2023-03-17 17:29:37 +0530

    first

---
+++ src/main/java/com/example/Travelbooking/Train.java
@@ -0,0 +1,11 @@
+package com.example.Travelbooking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Train {
+    @GetMapping("/mytrain")
+    public String getData() {
+        return "Please book you Train ticket";
+    }
+}
---
+++ src/main/java/com/example/Travelbooking/flight.java
@@ -0,0 +1,11 @@
+package com.example.Travelbooking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class flight {
+    @GetMapping("/myflight")
+    public String getData() {
+        return "Please book you flight ticket";
+    }
+}
